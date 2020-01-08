965c93ae47c2e8a8abbfe5a324a5e006f89b1fd3
Brianna Jordan <bryvjordan9@gmail.com>
2020-01-08 02:01:52 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,4 +1,19 @@
 class Cat
   attr_accessor :name, :meow
 
-end 
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def meow=(meow)
+    @meow = meow
+  end
+
+  def meow
+    @meow
+  end 
+end
